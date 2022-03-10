9b6f875dd24af367f3f0f6722cbcb385e753899d
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-10 13:59:33 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Return: Always 0 (Success)
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle\n");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }			
