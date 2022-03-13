83623c8aee42c9518daf560d5ebb20c7ddd35139
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-13 05:25:04 -0700

    Created a file 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	j = 48;
+
+	while (j < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (j != i && j < i)
+			{
+				putchar(j);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
