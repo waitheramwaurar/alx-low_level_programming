1862260ba55155bb302b8eb7dc0e8083fa914029
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-13 04:31:21 -0700

    Created a file 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers startin from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
