ecf58ac4598274d347ce97da1295ca83ff76ad07
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-13 05:08:00 -0700

    Created file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <=57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
