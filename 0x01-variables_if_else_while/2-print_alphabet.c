3dd932d35f208906376e277f344630f83c2ab70a
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-13 03:44:24 -0700

    When I cant fall asleep, I play what i call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main: prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 Variables, if, else, while
 0-positive_or_negative.c: checks whether a number is positive or negative
 1-last_digit: prints the last digit of a random number
+3-print_alphabet.c: prints the alphabet in lowercase
