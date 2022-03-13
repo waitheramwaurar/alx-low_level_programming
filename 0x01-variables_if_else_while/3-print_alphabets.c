21f04238406b48212f6728a50081093cfa075fe4
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-13 03:56:30 -0700

    Created file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase then uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabetsmall[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alphabetcapital[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i, j;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabetsmall[i];
+	}
+	for (j = 0; j < 26; j++)
+	{
+		putchar(alphabetcapital[j];
+	}
+	putchar('\n');
+}
