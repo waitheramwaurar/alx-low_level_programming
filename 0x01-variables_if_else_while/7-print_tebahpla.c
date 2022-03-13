d99e005864ae430cabbeacffef16ea0baaac81ce
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-13 04:40:43 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet in reverse order
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i <= 97; i--);
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
