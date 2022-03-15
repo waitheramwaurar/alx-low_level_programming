1ecc6efa1a283d40cf66953e4665e3ef43188fa7
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-15 13:59:45 -0700

    Created file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,18 @@
 
 int print_last_digit(int i)
 {
-	int last_digit = i % 10;
-	return (last_digit);
+	int last_digit;
+ 	
+	last_digit = i % 10;
+
+	if (last_digit < 0)
+	{
+		_putchar(-last_digit + 48);
+		return (-last_digit);
+	}
+	else
+	{
+		_putchar(last_digit + 48);
+		return (last_digit);
+	}
 }
