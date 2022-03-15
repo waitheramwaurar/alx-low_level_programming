78c64f7da1fec5295cfa08dd5bb02c53dfe7d346
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-15 10:17:42 -0700

    Created file 0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,3 @@
 #include <stdio.h>
 
-/**
- * main - contains function prototypes
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	int _putchar(char c);
-	return 0;
-}
+int _putchar(char);
