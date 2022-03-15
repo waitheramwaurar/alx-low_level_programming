a5a619bb9a0aa573f28af99e33c8060f82f35094
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-15 13:10:41 -0700

    Created file 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,10 +3,9 @@
 /**
  * _isalpha - function that checks for alphabetic character
  *
- * @c - character being checked, in ASCII code
+ * @c: character being checked, in ASCII code
  *
- * Return: 1 if @c is a letter, lowercase or uppercase
- * Return: 0 otherwise
+ * Return: 1 if @c is a letter, lowercase or uppercase, 0 otherwise
  */
 
 int _isalpha(int c)
