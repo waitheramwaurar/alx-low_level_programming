6e860bd3d7b00d39b22dbc52745b01b3cd965c99
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-21 15:49:04 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/** _strcpy - copies string
+ *
+ * @dest: pointer to destination
+ * @src: pointer to source
+ *
+ * Return: copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
