75b0ab7b31c755e2b08b765522ee848abf930f10
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-21 15:09:18 -0700

    Created a file 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string  to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int a, b;
+
+	for (a = 0; a < _strlen(s) / 2; a++)
+	{
+
+	}
+}
