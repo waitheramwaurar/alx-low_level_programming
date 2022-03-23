d704fb10cab0e91fd06a90e3d24acc2ce4cf240e
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-23 06:12:11 -0700

    Added file 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: the string thats being copied to 
+ * @src: the string that is being copied from
+ * @n: the number of bytes from src
+ *
+ * Return: the copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+        int i, j;
+
+        i = 0;
+
+        j = 0;
+
+        while (j < n)
+        {
+                dest[i] = src[j];
+                if (src[j] == '\0')
+                        break;
+                i++;
+                j++;
+        }
+
+        return (dest);
+}
