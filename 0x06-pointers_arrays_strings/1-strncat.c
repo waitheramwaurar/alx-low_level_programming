f71105476b82914534eed4dba44b2c8f2a0e81be
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-03-24 09:16:05 -0700

    Corrected file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,26 +12,26 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i, j;
+	int i, j;
 
-        i = 0;
+	i = 0;
 
-        j = 0;
+	j = 0;
 
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-        while (j < n)
-        {
-                dest[i] = src[j];
-                if (src[j] == '\0')
-                        break;
-                i++;
-                j++;
-        }
+	while (j < n)
+	{
+		dest[i] = src[j];
+		if (src[j] == '\0')
+			break;
+		i++;
+		j++;
+	}
 
-        return (dest);
+	return (dest);
 }
 
