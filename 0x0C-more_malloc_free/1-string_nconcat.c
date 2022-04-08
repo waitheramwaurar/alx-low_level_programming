9d5eac5fa6916481c9296922ccaa6fa4d1901ed4
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-04-08 00:10:09 -0700

    Finally WORKSgit add 1-string_nconcat.c!

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: amount of bytes.
+ * string_nconcat - concatenates two strings
  *
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * @s1: first string
+ * @s2: the second string
+ * @n: number of bytes of s2
+ *
+ * Return: concatenated string
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	char *new_str;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -21,29 +22,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
 
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
+	i = 1;
+	while (*(s1 + i))
+		i++;
 
-	if (n > ls2)
-		n = ls2;
+	j = 1;
+	while (*(s2 + j))
+		j++;
 
-	lsout = ls1 + n;
+	if (n > j)
+		n = j;
 
-	sout = malloc(lsout + 1);
+	new_str = malloc(sizeof(char) * (i + n + 1));
 
-	if (sout == NULL)
+	if (new_str == NULL)
 		return (NULL);
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
+	i = 0;
+	while (*(s1 + i))
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
 
-	sout[i] = '\0';
+	j = 0;
+	while (j < n)
+	{
+		new_str[i] = s2[j];
+		i++;
+		j++;
+	}
+	
+	new_str[i] = '\0';
 
-	return (sout);
+	return (new_str);
 }
