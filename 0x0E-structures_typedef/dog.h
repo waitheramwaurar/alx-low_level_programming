caf2a0538ce5dd33ecf435e28745dc955afee663
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-04-12 09:40:30 -0700

    Excited for structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - structure for a dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: the structure is about a dog's details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
