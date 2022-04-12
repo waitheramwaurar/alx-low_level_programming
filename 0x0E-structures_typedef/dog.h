f3e19c016c582e0fec0344a471295d39a2f335a0
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-04-12 12:57:58 -0700

    Added typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define _DOG_H_
 
 /**
+ * dog_t - Typedef for for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - structure for a dog
  * @name: name of the dog
  * @age: age of the dog
@@ -15,5 +20,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
