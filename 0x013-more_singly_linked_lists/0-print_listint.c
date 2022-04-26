6cc9845e87160d1b230fea293f158ff0c71e6909
waitheramwaurar <rwaitheramwaura@gmail.com>
2022-04-26 22:55:07 +0300

    Number of nodes

---
+++ 0x013-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a list
+ *
+ * @h: the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int num_of_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n",h->n);
+		h = h->next
+		num_of_nodes++;
+	}
+	return (num_of_nodes);
+}
